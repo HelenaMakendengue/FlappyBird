70bb2bc7f04c795231cb3083bab9d4d69c7dff82
Helena Makendengue <HelenaMakens1013@gmail.com>
2018-09-23 02:14:10 -0400

    added top score feature

---
+++ FlappyBird/Assets/Scripts/TopScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopScore : MonoBehaviour {
+
+    public static int HIGHSCORE = 0;
+
+    public Text text;
+
+    void Update () {
+        if(ScoreManger.SCORE > HIGHSCORE) {
+            HIGHSCORE = ScoreManger.SCORE;
+        }
+        text.text = "Top Score: " + HIGHSCORE;
+    }
+}
